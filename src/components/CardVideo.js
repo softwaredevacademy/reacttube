60cb94461f73e74cb358d67107abe10f3edaf031
eduardo <eduardoa@kth.se>
2020-03-25 23:11:58 +0100

    added css and more images

--- src/components/CardVideo.js
+++ src/components/CardVideo.js
@@ -5,10 +5,13 @@ class CardVideo extends Component {
 	// Render
 	render() {
 		return (
-			<a href={this.props.prop.link} target="_blank">
-				<h1>{this.props.prop.title}!!!</h1>
+			<article className="CardVideo">
+				<a href={this.props.prop.link} target="_blank">
+					<img src={require('../images/' + this.props.prop.image)}></img>
+				</a>
+				<h1>{this.props.prop.title}</h1>
 				<p>{this.props.prop.description}</p>
-			</a>
+			</article>
 		)
 	}
 }
