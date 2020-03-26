7bfa7f3941aa29a90a3a84759eca3267955e45d7
eduardo <eduardoa@kth.se>
2020-03-26 21:10:45 +0100

    added css for video

--- src/templates/VideoPage.js
+++ src/templates/VideoPage.js
@@ -22,9 +22,23 @@ function VideoPage({ match }) {
 
       <div className="body-container">
         <h1 className="title">{video.title}</h1>
-        <p className="author">{video.author}</p>
-        <p className="description">{video.description}</p>
+        <p className="description">{video.views} views • {video.uploadDate}</p>
+
         <hr />
+
+        <div className="meta-data">
+          <div className="left" >
+            <img className="channel-thumb" src={require('../images/channels/' + video.channelThumb + '.jpg')} alt="Channel thumbnail" />
+          </div>
+          <div className="right">
+            <h1 className="title">{video.channelName}</h1>
+            <p className="description">{video.suscribers} suscribers</p>
+            <p className="description">{video.description}</p>
+          </div>
+        </div>
+
+        <hr />
+
         <Link className="button" to="/">Back to home</Link>
       </div>
     </div>
